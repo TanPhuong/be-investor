fc4b97e67c2c6fa01b4ffb8718f110a2a0262a31
VIETNAM TOURIST <tanphuong2307@gmail.com>
2024-11-27 15:03:13 +0700

    adjust config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
-const router = require('./api/router.js')
+const router = require('./src/router.js')
 
 dotenv.config()
 
