6d8edcd87c9344d4cb762e3d28f49f586dcca7ea
VIETNAM TOURIST <tanphuong2307@gmail.com>
2024-11-27 14:23:57 +0700

    update vercel.json

--- src/api/router.js
+++ src/api/router.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { sendEmailController } = require('./EmailService');
+const { sendEmailController } = require('../EmailService');
 
 const router = express.Router();
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
-const router = require('./router.js')
+const router = require('./api/router.js')
 
 dotenv.config()
 
